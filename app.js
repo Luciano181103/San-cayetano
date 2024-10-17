5c39eef3f51d63fa10fa3f4761be14fb04b957f0
Luciano181103 <lucianoledesma181103@gmail.com>
2024-10-17 01:58:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 const products = [
-    { id: 1, name: "Producto 1", price: 100, description: "Descripción 1", image: "https://dl.dropboxusercontent.comscl/fi/woy3wurd6bwxgonm27tk7/San-Cayetano.png?rlkey=ucgzjtd9fedgy0dsc2uo4yowo&st=2erpfhfp&dl=0" },
-    { id: 2, name: "Producto 2", price: 200, description: "Descripción 2", image: "https://via.placeholder.com/150" },
-    { id: 3, name: "Producto 3", price: 150, description: "Descripción 3", image: "https://via.placeholder.com/150" }
+    { id: 1, name: "Hamburguesa Comun", price: 1500, description: "Pan,mayonesa, lechuga, tomate, carne, queso, jamon (opcional a pedir: Salsa de ajo, salsa criolla)", category: "Hamburguesas" },
+    { id: 2, name: "Hamburguesa Especial", price: 2000, description: "Pan, mayonesa, lechuga, tomate, carne, queso,huevo, jamon (opcional a pedir: Salsa de ajo, salsa criolla)", category: "Hamburguesas" },
+    { id: 3, name: "Hamburguesa Super", price: 2500, description: "Pan, mayonesa, lechuga, tomate, carne x2, queso x2, morron,huevo, jamon, mostaza (opcional a pedir: Salsa de ajo, salsa criolla)", category: "Hamburguesas" },
+    { id: 4, name: "Pizza Comun", price: 3200, description: "Salsa, queso, troso de jamon, aceituna", category: "Pizzas" },
+    { id: 5, name: "Pizza Especial", price: 400, description: "Salsa, queso, jamon, aceituna, huevo", category: "Pizzas" },
+    { id: 6, name: "Pizza Super", price: 4500, description: "Salsa, jamon, queso, huevo, morron, aceituna", category: "Pizzas" },
+    { id: 7, name: "Pizza Mozarella", price: 4000, description: "Pizza con mozzarella", category: "Pizzas" },
+    { id: 8, name: "Pizza Calaresa", price: 4200, description: "Pizza con salame", category: "Pizzas" },
+    { id: 9, name: "Pizza a Caballo", price: 4500, description: "Pizza con huevo frito", category: "Pizzas" },
+    { id: 10, name: "Pizza Napolitana", price: 4500, description: "Pizza con Jamon, tomate, huevo, aceituna", category: "Pizzas" },
+    { id: 11, name: "Pizzanesa con papas", price: 12000, description: "Pizza rellena con tomate, milanesa, queso, cubierta con salsa, huevos fritos, papas fritas ", category: "Pizzas" },
+    { id: 12, name: "Salchipizza", price: 4000, description: "Pizza con salchichas y huevo", category: "Pizzas" },
+    { id: 13, name: "Pizza Cuatro quesos", price: 4500, description: "Salsa, queso cremoso, mozarella, parmesano, roquefort", category: "Pizzas" },
+    { id: 14, name: "Sándwich de milanesa", price: 5000, description: "Sándwich con lechuga, tomate, milanesa, huevo, jamon y queso", category: "Varios" },
+    { id: 15, name: "1/2 Sándwich de milanesa", price: 3000, description: "Medio Sándwich con lechuga, tomate, milanesa, huevo, jamon y queso", category: "Varios" },
+    { id: 16, name: "Figazza", price: 5000, description: "Pan, lechuga, tomate, carne, huevo, queso, jamon, recubierto con mozarella", category: "Varios" },
+    { id: 17, name: "Lomito", price: 5000, description: "Pan, lechuga, tomate, lomito, queso, huevo, jamon, recubierto con mozarella", category: "Varios" },
+    { id: 18, name: "Hamburpizza", price: 9000, description: "Pizza rellena de tomate, carne, queso, huevo,tapa, salsa, queso, jamon, huevo y aceituna", category: "Varios" },
+    { id: 19, name: "1/2 Hamburpizza", price: 5000, description: "Media pizza con base de hamburguesa", category: "Varios" },
+    { id: 20, name: "Milanesa c/papas", price: 4100, description: "Milanesa con papas fritas", category: "Varios" },
+    { id: 21, name: "Milanesa Napolitana c/papas", price: 4500, description: "Milanesa con salsa, jamon, queso y papas", category: "Varios" },
+    { id: 22, name: "Milanesa a Caballo c/papas", price: 45000, description: "Milanesa con huevo frito y papas", category: "Varios" },
+    { id: 23, name: "Tarta de jamón y queso", price: 7000, description: "Tarta con jamón y queso", category: "Varios" },
+    { id: 24, name: "Empanadas (Jamón y queso - Carne)", price: 5500, description: "Empanadas de jamón y queso o carne", category: "Varios" },
+    { id: 25, name: "1/2 Empanadas (Jamón y queso - Carne)", price: 3000, description: "Media docena de empanadas", category: "Varios" },
+    { id: 26, name: "Porción de papas", price: 2500, description: "Porción de papas fritas", category: "Varios" },
+    { id: 27, name: "Porción de papas c/cheddar", price: 3800, description: "Papas fritas con cheddar", category: "Varios" },
+    { id: 28, name: "Pancho", price: 1500, description: "Pancho (hot dog)", category: "Varios" },
+    { id: 29, name: "Carlito simple", price: 2000, description: "Pan miga, queso y jamon", category: "Varios" },
+    { id: 30, name: "Carlito Triple", price: 3000, description: "Pan miga, lechuga, tomate, huevo, pan, jamon y queso", category: "Varios" },
+    { id: 31, name: "3 Hambuguesas Comunes", price: 4000, description: "3 Hambuguesas Comunes", category: "Promociones" },
+    { id: 32, name: "3 Hamburguesas Especiales", price: 5000, description: "3 Hamburguesas Especiales", category: "Promociones" },
+    { id: 33, name: "2 Pizzas Comunes", price: 6000, description: "2 Pizzas Comunes", category: "Promociones" },
+    { id: 34, name: "2 Pizzas Especiales", price: 7000, description: "Carlito triple", category: "Promociones" },
+    { id: 35, name: "1 Hamburguesa Especial + 1 porcion de papas", price: 4000, description: "1 Hamburguesa Especial + 1 porcion de papas", category: "Lunes a Jueves" },
+    { id: 36, name: "1 Pizza Mozarella + 1/2 docena de Empanadas", price: 6500, description: "1 Pizza Mozarella + 1/2 docena de Empanadas", category: "Lunes a Jueves" },
+    { id: 37, name: "3 Hamburguesas Especiales + Pizza Especial", price: 6500, description: "3 Hamburguesas Especiales + Pizza Especial", category: "Lunes a Jueves" },
+    { id: 38, name: "1 Sándwich + 1 Pizza Especial", price: 8000, description: "1 Sándwich + 1 Pizza Especial", category: "Lunes a Jueves" },
+    { id: 39, name: "1 Figazza + 1 Porción de Papas", price: 7000, description: "1 Figazza + 1 Porción de Papas", category: "Lunes a Jueves" },
+    { id: 40, name: "1 Sándwich + 1 Porcion de papas", price: 6500, description: "1 Sándwich + 1 Porcion de papas", category: "Lunes a Jueves" },
+
 ];
 
 let cart = [];
-let orderNumber = 1; // Inicializar número de orden en 1
+let orderNumber = 1;
 
 // Función para reiniciar el número de orden a las 13:00 hs
 function checkForOrderReset() {
@@ -13,24 +51,23 @@ function checkForOrderReset() {
     const currentHour = now.getHours();
     const currentMinutes = now.getMinutes();
     
-    // Reiniciar el contador si es exactamente las 13:00 hs
     if (currentHour === 13 && currentMinutes === 0) {
-        orderNumber = 1; // Reiniciar número de orden a 1
-        console.log('Número de orden reiniciado a 1.');
+        orderNumber = 1;
     }
 }
 
-// Ejecutar checkForOrderReset cada minuto para verificar la hora
 setInterval(checkForOrderReset, 60000);
 
-function displayProducts() {
+function displayProducts(productsToDisplay) {
     const productList = document.getElementById('product-list');
-    products.forEach(product => {
+    productList.innerHTML = '';  // Limpiar el contenedor de productos
+
+    productsToDisplay.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('col-md-4', 'mb-4');
         productDiv.innerHTML = `
             <div class="card">
-                <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                <img src="${product.image}" class="card-img-top" alt="${product.name}" style="max-height: 150px; object-fit: contain;">
                 <div class="card-body">
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text">${product.description}</p>
@@ -41,6 +78,19 @@ function displayProducts() {
         `;
         productList.appendChild(productDiv);
     });
+}
+
+// Mostrar todos los productos al cargar la página
+displayProducts(products);
+
+// Función para filtrar productos por categoría
+function filterByCategory(category) {
+    if (category === 'All') {
+        displayProducts(products);
+    } else {
+        const filteredProducts = products.filter(product => product.category === category);
+        displayProducts(filteredProducts);
+    }
 }
 
 function addToCart(productId) {
@@ -102,7 +152,7 @@ function sendWhatsAppMessage() {
         return;
     }
 
-    let message = 'Pedido de Mostachos:\n\n';
+    let message = 'Pedido:\n\n';
     let total = 0;
 
     cart.forEach(item => {
@@ -111,18 +161,16 @@ function sendWhatsAppMessage() {
     });
 
     message += `\nTotal del pedido: $${total}\n`;
-
     
 
-    const isDelivery = document.getElementById('delivery-option').checked;
-    if (isDelivery) {
-        message += 'Con envío a domicilio. Por favor, envíe su ubicación.';
+    const deliveryOption = document.getElementById('delivery-option');
+    if (deliveryOption.checked) {
+        message += 'Con envío a domicilio. Por favor, envíe su ubicación.\n';
+    } else {
+        message += 'Sin envío a domicilio.\n';
     }
 
-    const whatsappURL = `https://api.whatsapp.com/send?phone=5493794214144&text=${encodeURIComponent(message)}`;
-
-    window.open(whatsappURL, '_blank');
+    orderNumber++; // Incrementar el número de orden
+    const whatsappUrl = `https://wa.me/5493794214144?text=${encodeURIComponent(message)}`;
+    window.open(whatsappUrl, '_blank');
 }
-
-document.getElementById('cart-button').addEventListener('click', viewCart);
-displayProducts();
